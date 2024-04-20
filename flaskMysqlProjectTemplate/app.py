7fe94f9e6e5c18ca75ff71b0c69a74aa036047b8
oliveirasilm1 <oliveirasilm1@montclair.edu>
2024-04-20 12:27:47 -0400

    BasePages 4/20
    + Log In (fully implemented)
    + Log Out (fully implemented)
    + Registration (fully implemented)
    + Courses Page for Logged in students
    + Profile Page for Logged in students
    + Base Schema established

--- flaskMysqlProjectTemplate/app.py
+++ flaskMysqlProjectTemplate/app.py
@@ -1,10 +1,13 @@
 import sqlparse
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 from MySQLConfig import MySQLConfig
+import re
 
 app = Flask(__name__)
 
+app.secret_key = 'CSIT355MOEGMH'
+# Testing GitFile Update
 # Configure MySQL using the imported config
 app.config['MYSQL_HOST'] = MySQLConfig.HOST
 app.config['MYSQL_USER'] = MySQLConfig.USER
@@ -40,16 +43,120 @@ with app.app_context():
 #     execute_initial_sql()
 #     pass
 
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM Students WHERE username = %s AND password = %s', (username, password,))
+        # Fetch one record and return the result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account[0] # Access corresponding member in tuple
+            session['username'] = account[1]
+            # Redirect to home page
+            return redirect(url_for('userpage'))
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    return render_template('index.html', msg=msg)
 
-@app.route('/')
-def index():
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM Students WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        # Check for valid email and username
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesn't exist, and the form data is valid, so insert the new account into the accounts table
+            cursor.execute('INSERT INTO Students VALUES (NULL, %s, %s, %s, 0, %s, %s, 5, 4)', (username, password, email, 'Test', 'Test'))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+
+
+@app.route('/userpage')
+def userpage():
+    # Check if the user is logged in
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM Courses")
+        data = cursor.fetchall()
+        cursor.close()
+        #    return render_template('index.html', data=data)
+        return render_template('home.html', username=session['username'], data=data)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
+# http://localhost:5000/pythinlogin/profile - this will be the profile page, only accessible for logged in users
+@app.route('/profile')
+def profile():
+    # Check if the user is logged in
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM Students WHERE id = %s', (session['id'],))
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not logged in redirect to login page
+    return redirect(url_for('login'))
+
+@app.route('/userpage/enroll')
+def enroll():
+    return redirect(url_for('profile'))
+
+@app.route('/userpage/withdraw')
+def withdraw():
+    return redirect(url_for('profile'))
+
+# ORIGINAL INDEX PAGE!!!!
+#@app.route('/')
+#def index():
 
     # Display a list of records from the database
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM student")
-    data = cursor.fetchall()
-    cursor.close()
-    return render_template('index.html', data=data)
+#    cursor = mysql.connection.cursor()
+#    cursor.execute("SELECT * FROM student")
+#    data = cursor.fetchall()
+#    cursor.close()
+#    return render_template('index.html', data=data)
 
 # Route for adding students
 @app.route('/add', methods=['POST'])
--- flaskMysqlProjectTemplate/static/sql/init.sql
+++ flaskMysqlProjectTemplate/static/sql/init.sql
@@ -1,19 +1,59 @@
-Drop table if exists student;
+drop table if exists Enrolled;
+drop table if exists Students;
+drop table if exists Courses;
 
--- Create the student table if it doesn't exist
+CREATE TABLE Students(
+  id int(10) NOT NULL AUTO_INCREMENT,
+  username varchar(50) NOT NULL,
+  password varchar(255) NOT NULL,
+  email varchar(100) NOT NULL,
+  admin bool,
+  firstname varchar(50),
+  lastname varchar(50),
+  age int,
+  gpa real,
+  PRIMARY KEY(id));
 
-CREATE TABLE IF NOT EXISTS student (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    sname VARCHAR(255) NOT NULL,
-    age INT,
-    email VARCHAR(255) NOT NULL
+CREATE TABLE Courses(
+  cid varchar(4),
+  cname varchar(50),
+  credits int,
+  description varchar(255),
+  filledslots int,
+  totalslots int,
+  PRIMARY KEY(cid)
 );
 
-DELETE from student;
+CREATE TABLE Enrolled(
+  id int,
+  cid varchar(4),
+  grade real,
+  PRIMARY KEY (id, cid),
+  FOREIGN KEY (id) REFERENCES Students(id),
+  FOREIGN KEY (cid) REFERENCES Courses(cid)
+);
 
--- Insert 5 fake student records
-INSERT INTO student (sname, age, email) VALUES ('John Doe', 19, 'john@example.com');
-INSERT INTO student (sname, age, email) VALUES ('Jane Smith', 21, 'jane@example.com');
-INSERT INTO student (sname, age, email) VALUES ('Alice Johnson',22, 'alice@example.com');
-INSERT INTO student (sname, age, email) VALUES ('Bob Brown', 20, 'bob@example.com');
-INSERT INTO student (sname, age, email) VALUES ('Eve Davis', 18, 'eve@example.com');
+delete from Students;
+INSERT INTO Students (id, username, password, email, admin, firstname, lastname, age, gpa) VALUES
+  (1, 'jacobsa1', 'abc123', 'jacobsa1@north.edu', 0, 'Alice', 'Jacobs', 19, 3.1),
+  (2, 'nealj1', 'def456', 'nealj1@north.edu', 0, 'Julian', 'Neal', 21, 3.2),
+  (3, 'pateld1', 'ghi789', 'pateld1@north.edu', 1, 'Dev', 'Patel', 26, NULL),
+  (4, 'perreiram1', 'jkl012', 'perreiram1@north.edu', 0, 'Melissa', 'Perreira', 22, 3.4),
+  (5, 'jacobsa1', 'mno345', 'jacobsa1@north.edu', 0, 'Alice', 'Jacobs', 19, 3.1);
+
+delete from Courses;
+INSERT INTO Courses (cid, cname, credits, description, filledslots, totalslots) VALUES
+  ('C001', 'Calculus', 4, '...', 1, 24),
+  ('C002', 'Biology', 3, '...', 2, 25),
+  ('C003', 'Linear Algebra', 3, '...', 3, 20),
+  ('C004', 'Chemisty', 4, '...', 1, 30);
+
+delete from Enrolled;
+INSERT INTO Enrolled (id, cid, grade) VALUES
+  (1, 'C001', 89.3),
+  (5, 'C002', 78.8),
+  (2, 'C003', 90.1),
+  (2, 'C004', 81.6),
+  (1, 'C002', 81.4),
+  (5, 'C003', 83.2),
+  (4, 'C003', 94.7);
